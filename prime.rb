0bcc592dcdf9314014cb5c2f45db4b19dba5534e
David Ingram <david@ingramaviation.com>
2019-08-15 20:28:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 # # Add  code here!
 def prime?(x)
   array = (2..x).to_a 
-  if x < 2 
+  if x < 2
     return false
   elsif array.any? {|num| x % num == 0} 
       return false 
+  elsif x == 2 
+      return true 
   else
       return true 
   end
