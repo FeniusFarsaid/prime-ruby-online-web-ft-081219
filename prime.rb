0f11b2bcf4b98ba49c07ed53e2bccd8465904bb3
David Ingram <david@ingramaviation.com>
2019-08-15 19:51:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
-# Add  code here!
-def prime?(x)
-  if x < 2 
-    return false
-  else
-      if x % y(2..x) == 0 
-      return false 
-  elsif
-      return true 
-  end
-end 
+# # Add  code here!
+# def prime?(x)
+#   if x < 2 
+#     return false
+#   else
+#       if x % y(2..x) == 0 
+#       return false 
+#   elsif
+#       return true 
+#   end
+# end 
 
