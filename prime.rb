b936c53bfc0af524d10a9a590e91b5b701bb9535
David Ingram <david@ingramaviation.com>
2019-08-15 21:32:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,4 @@
 # # Add  code here!
-# def prime?(x)
-#   array = (2..x).to_a 
-#   if x < 2
-#     return false
-#   elsif x <= 3 
-#       return true
-#   elsif array.any? {|num| x % num == 0} 
-#       return false 
-#   else
-#       return true 
-#   end
-# end 
-
 def prime?(x)
   if x <= 1
     return false
