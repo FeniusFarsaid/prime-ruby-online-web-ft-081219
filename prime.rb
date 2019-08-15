ea671e914045b73189ac8729c322d1fb01a244aa
David Ingram <david@ingramaviation.com>
2019-08-15 21:25:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,24 @@
 # # Add  code here!
-def prime?(x)
-  array = (2..x).to_a 
-  if x < 2
-    return false
-  elsif x <= 3 
-      return true
-  elsif array.any? {|num| x % num == 0} 
-      return false 
-  else
-      return true 
-  end
-end 
+# def prime?(x)
+#   array = (2..x).to_a 
+#   if x < 2
+#     return false
+#   elsif x <= 3 
+#       return true
+#   elsif array.any? {|num| x % num == 0} 
+#       return false 
+#   else
+#       return true 
+#   end
+# end 
 
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
+  end
+end
