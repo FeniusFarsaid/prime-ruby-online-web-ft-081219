a3ae12b0bb77b5c8825450cf872c668fdb507a4e
David Ingram <david@ingramaviation.com>
2019-08-15 19:33:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,19 @@
 # Add  code here!
 def prime?(num)
+  if num < 2 
+    false
+  else
+    for i 
   
   
   
 end 
 
-def is_prime(x):
-    if x < 2:
-        return False
-    else:
-        for n in range(2,x):
-            if x % n == 0:
-               return False
-        return True
+# def is_prime(x):
+#     if x < 2:
+#         return False
+#     else:
+#         for n in range(2,x):
+#             if x % n == 0:
+#               return False
+#         return True
