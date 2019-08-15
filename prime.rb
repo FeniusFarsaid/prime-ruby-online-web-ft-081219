26d5d71c2c98283172f3f77d2f118022d3bf9fcc
David Ingram <david@ingramaviation.com>
2019-08-15 20:21:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(x)
   if x < 2 
     return false
   elsif
-      if array.any? {|num| x % num == 0} 
+      array.any? {|num| x % num == 0} 
       return false 
   else
       return true 
